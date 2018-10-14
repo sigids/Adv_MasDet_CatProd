81523f401e7a538b62968639beb96f21992dd137
sigid sumargo <sigid.sumargo@gmail.com>
2018-10-14 22:32:36 +0700

    initial commit

---
+++ Adv_MasDet_CatProd/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adv_MasDet_CatProd.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getProductCategories()
+        {
+            List<Category> categories = new List<Category>();
+            using (TestMasDetEntities dc = new TestMasDetEntities())
+            {
+                categories = dc.Categories.OrderBy(a => a.CategoryName).ToList();
+            }
+            return new JsonResult { Data = categories, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult getProducts(int categoryID)
+        {
+            List<Product> products = new List<Product>();
+            using (TestMasDetEntities dc = new TestMasDetEntities())
+            {
+                products = dc.Products.Where(a => a.CategoryID.Equals(categoryID)).OrderBy(a => a.ProductName).ToList();
+            }
+            return new JsonResult { Data = products, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpPost]
+        public JsonResult save(OrderMaster order)
+        {
+            bool status = false;
+            DateTime dateOrg;
+            var isValidDate = DateTime.TryParseExact(order.OrderDate.ToShortDateString(), "mm-dd-yyyy", null, System.Globalization.DateTimeStyles.None, out dateOrg);
+            if (isValidDate)
+            {
+                order.OrderDate = dateOrg;
+            }
+
+            var isValidModel = TryUpdateModel(order);
+            if (isValidModel)
+            {
+                using (TestMasDetEntities dc = new TestMasDetEntities())
+                {
+                    dc.OrderMasters.Add(order);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+    }
+}
